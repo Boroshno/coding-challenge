63656fee8179a3a558f93e42385418976ac7f3d2
DmytroLytvynenko <dmytro.lytvynenko@beetroot.se>
2021-09-18 22:39:13 +0300

    Tests fixed as I think they should be

--- ConstructionLine.CodingChallenge.Tests/SearchEngineTestsBase.cs
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTestsBase.cs
@@ -39,7 +39,8 @@ namespace ConstructionLine.CodingChallenge.Tests
 
                 var expectedSizeCount = shirts
                     .Count(s => s.Size.Id == size.Id
-                                && (!searchOptions.Colors.Any() || searchOptions.Colors.Select(c => c.Id).Contains(s.Color.Id)));
+                                && ((!searchOptions.Sizes.Any() || searchOptions.Sizes.Select(c => c.Id).Contains(s.Size.Id))
+                                    && (!searchOptions.Colors.Any() || searchOptions.Colors.Select(c => c.Id).Contains(s.Color.Id))));
 
                 Assert.That(sizeCount.Count, Is.EqualTo(expectedSizeCount), 
                     $"Size count for '{sizeCount.Size.Name}' showing '{sizeCount.Count}' should be '{expectedSizeCount}'");
@@ -57,8 +58,9 @@ namespace ConstructionLine.CodingChallenge.Tests
                 Assert.That(colorCount, Is.Not.Null, $"Color count for '{color.Name}' not found in results");
 
                 var expectedColorCount = shirts
-                    .Count(c => c.Color.Id == color.Id  
-                                && (!searchOptions.Sizes.Any() || searchOptions.Sizes.Select(s => s.Id).Contains(c.Size.Id)));
+                    .Count(c => c.Color.Id == color.Id
+                                && ((!searchOptions.Sizes.Any() || searchOptions.Sizes.Select(s => s.Id).Contains(c.Size.Id))
+                                    && (!searchOptions.Colors.Any() || searchOptions.Colors.Select(c => c.Id).Contains(c.Color.Id))));
 
                 Assert.That(colorCount.Count, Is.EqualTo(expectedColorCount),
                     $"Color count for '{colorCount.Color.Name}' showing '{colorCount.Count}' should be '{expectedColorCount}'");
